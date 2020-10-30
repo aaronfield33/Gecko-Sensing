d958ba7c8f5537b843685b67d72101bc2db73be4
aaronfield33 <73485410+aaronfield33@users.noreply.github.com>
2020-10-30 10:30:58 -0500

    Add files via upload

---
+++ Losant_Dual_Sensor.ino
@@ -0,0 +1,149 @@
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Adafruit_Sensor.h>
+#include "DHT.h"
+#include <ESP8266HTTPClient.h>
+#include <Losant.h>
+#include "internal/LosantDevice.h"
+#include <time.h>
+#include <WifiClientSecure.h>
+
+#define MQTT_MAX_PACKET_SIZE 256
+
+// Wifi Credentials
+const char* WIFI_SSID = "ASUS";
+const char* WIFI_PASS = "123456679";
+
+// Losant credentials.
+const char* LOSANT_DEVICE_ID = "5f9b04935a36a500063d54ed";
+const char* LOSANT_ACCESS_KEY = "c388c3b6-a8b3-411d-89f2-837c130083b7";
+const char* LOSANT_ACCESS_SECRET = "581ea57169367808bcd1fcc353391424440f4b847ce6e0c05d20ba709a4190f0";
+
+DHT dht1(D2, DHT22);
+DHT dht2(D3, DHT22);
+
+float t1 = 0.0;
+float h1 = 0.0;
+float t2 = 0.0;
+float h2 = 0.0;
+
+static const char digicert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
+QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
+CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
+nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
+43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
+T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
+gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
+BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
+TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
+DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
+hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
+06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
+PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
+YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
+CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
+-----END CERTIFICATE-----
+)EOF";
+
+BearSSL::WiFiClientSecure wifiClient;
+
+LosantDevice device(LOSANT_DEVICE_ID);
+device.connectSecure(wifiClient, LOSANT_ACCESS_KEY, LOSANT_ACCESS_SECRET);
+
+void setClock() {
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    now = time(nullptr);
+  }
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+}
+
+void connect() {
+
+  // Connect to Wifi.
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Validating  the SSL for for a secure connection you must
+  // set trust anchor, as well as set the time.
+  BearSSL::X509List cert(digicert);
+  wifiClient.setTrustAnchors(&cert);
+  setClock();
+  
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Connect to Losant.
+  Serial.println();
+  Serial.print("Connecting to Losant...");
+
+  device.connectSecure(wifiClient, LOSANT_ACCESS_KEY, LOSANT_ACCESS_SECRET);
+
+  while(!device.connected()) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected!");
+}
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  dht1.begin();
+  dht2.begin();
+
+  connect();
+}
+
+void report(double t1, double h1, double t2, double h2) {
+  StaticJsonDocument<1024> jsonbuffer;
+  jsonbuffer["t1"] = t1;
+  jsonbuffer["h1"] = h1;
+  jsonbuffer["t2"] = t2;
+  jsonbuffer["h2"] = h2;
+  device.sendState(jsonbuffer);
+  Serial.println("Reported!");
+}
+
+void loop() {
+  float t1 = dht1.readTemperature(true);
+  float h1 = dht1.readHumidity();
+  float t2 = dht2.readTemperature(true);
+  float h2 = dht2.readHumidity();
+
+  Serial.print("DHT Sensor 1   ");
+  Serial.print(t1); Serial.print(" F  ");
+  Serial.print(h1); Serial.print("%RH");
+  Serial.println();
+  Serial.print("DHT Sensor 2   ");
+  Serial.print(t2); Serial.print(" F  ");
+  Serial.print(h2); Serial.print("%RH");
+  Serial.println();
+
+  delay(2000);
+}
